f3fd222a5f578292c245fc3e2f177fb8e0c5f9e8
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-03-01 19:06:29 +0200

    push fetch to prod

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,17 @@
+import axios from 'axios';
+import {useEffect} from 'react';
+
+async function fetchMovie() {
+  const response = await axios(`http://www.wangchenga.cn/xcx/?id=AKSJ00002471`);
+  console.log(response);
+}
+
 export const Login = () => {
+  useEffect(() => {
+    const res = fetchMovie();
+    console.log(res);
+  }, []);
+
   return (
     <>
       <p>HERE GONNA BE MAIN CONTENT OF HOME PAGE</p>
